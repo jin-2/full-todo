4dbaac2a9e55954197eeb86c2d3ab87cee466fa5
이수진 <sujinlee2@jobkorea.co.kr>
2023-04-11 17:26:05 +0900

    Add write page, post request

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}))
 
 app.listen(8080, function() {
   console.log('listening on :8080');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
 });
 
 app.get('/pet', function (req, res) {
@@ -13,6 +18,11 @@ app.get('/beauty', function (req, res) {
   res.send('beauty에 관련 페이지')
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
+app.get('/write', function (req, res) {
+  res.sendFile(__dirname + '/write.html')
 });
+
+app.post('/add', (req, res) => {
+  console.log(req.body)
+  res.send('전송완료!')
+})
